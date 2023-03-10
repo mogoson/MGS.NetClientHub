a1487ef2f2ac1c12c35a1fd05c08fec9e423381b
LinLang <510525@nd.com>
2023-03-10 23:41:53 +0800

    Add NetMonoHub

---
+++ UnityProject/Assets/MGS.Packages/NetClientHub/Runtime/NetMonoHub/Implement/NetMonoHub.cs
@@ -0,0 +1,69 @@
+﻿/*************************************************************************
+ *  Copyright © 2023 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  NetMonoHub.cs
+ *  Description  :  Hub to manage net clients and cache net
+ *                  data, and unity main thread notify status.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  03/10/2023
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+using System.Collections;
+using UnityEngine;
+
+namespace MGS.Net
+{
+    /// <summary>
+    /// Hub to manage net clients and cache net data,
+    /// and main thread notify status.
+    /// </summary>
+    public class NetMonoHub : NetBridgeHub, INetMonoHub
+    {
+        /// <summary>
+        /// Behaviour for hub to StartCoroutine.
+        /// </summary>
+        protected MonoBehaviour behaviour;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="resultCacher">Cacher for net result.</param>
+        /// <param name="clientCacher">Cacher for net client.</param>
+        /// <param name="concurrency">Max count of concurrency clients.</param>
+        /// <param name="resolver">Net resolver to check retrieable.</param>
+        public NetMonoHub(ICacher<string> resultCacher = null,
+            ICacher<INetClient> clientCacher = null, int concurrency = 3, INetResolver resolver = null)
+            : base(resultCacher, clientCacher, concurrency, resolver)
+        {
+            behaviour = new GameObject("NetBehaviour").AddComponent<MonoBehaviour>();
+            behaviour.StartCoroutine(BehaviourTick());
+            Object.DontDestroyOnLoad(behaviour.gameObject);
+        }
+
+        /// <summary>
+        /// Dispose all resource.
+        /// </summary>
+        public override void Dispose()
+        {
+            base.Dispose();
+            Object.Destroy(behaviour.gameObject);
+            behaviour = null;
+        }
+
+        /// <summary>
+        /// Behaviour tick to update.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator BehaviourTick()
+        {
+            while (!isDisposed)
+            {
+                Update();
+                yield return null;
+            }
+        }
+    }
+}
---
+++ UnityProject/Assets/MGS.Packages/NetClientHub/Runtime/NetMonoHub/Interface/INetMonoHub.cs
@@ -0,0 +1,21 @@
+﻿/*************************************************************************
+ *  Copyright © 2023 Mogoson. All rights reserved.
+ *------------------------------------------------------------------------
+ *  File         :  INetMonoHub.cs
+ *  Description  :  Interface for hub to manage net clients and cache net
+ *                  data, and unity main thread notify status.
+ *------------------------------------------------------------------------
+ *  Author       :  Mogoson
+ *  Version      :  1.0
+ *  Date         :  03/10/2023
+ *  Description  :  Initial development version.
+ *************************************************************************/
+
+namespace MGS.Net
+{
+    /// <summary>
+    /// Interface for hub to manage net clients and cache net data,
+    /// and unity main thread notify status.
+    /// </summary>
+    public interface INetMonoHub : INetBridgeHub { }
+}
